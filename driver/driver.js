1aba91fe614354d42a15233003c2b917d06bf341
SBALDOCK <stephenbaldock78@gmail.com>
2020-09-03 07:59:10 -0700

    lab18 completion

--- driver/driver.js
+++ driver/driver.js
@@ -1,58 +1,26 @@
 'use strict';
 
 // const inquirer = require('inquirer');
-const net = require('net');
+// const net = require('net');
 
-const client = new net.Socket();
+const io = require('socket.io-client');
+const driverSocket = io.connect('http://localhost:3000/caps');
 
-const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || 3000;
-client.connect(port, host, () => {
-  console.log('Driver successfully connected to', host, ':', port);
-});
+driverSocket.emit('join', process.env.STORE_NAME);
 
-require('dotenv').config();
-
-client.on('data', buffer => {
-  let object = JSON.parse(buffer);
-
-  if(object.event === 'pickup') {
-    checkForPickup(object);
-  } else if (object.event === 'in-transit'){
-    console.log('in-transit');
-    simulateDelivery(object);
-  } 
-
-});
-
-function checkForPickup(object){
+driverSocket.on('pickup', payload => {
   setTimeout(() => {
-    console.log('picking up', object.payload.orderID);
-    let newOrder = JSON.stringify(orderInTransit(object.payload));
-    client.write(newOrder);
+    console.log('picking up', payload.orderID);
+    driverSocket.emit('in-transit', payload);
   }, 1000);
-  
-}
-
-function orderInTransit(object) {
-  const orderInfo = {
-    event: 'in-transit',
-    payload: object,
-  };
-  return orderInfo;
-}
-
-function simulateDelivery(object){
   setTimeout(() => {
-    let newOrder = JSON.stringify(deliveryConfirmation(object.payload));
-    client.write(newOrder);
+    console.log('Delivering', payload.orderID);
+    driverSocket.emit('delivered', payload);
   }, 3000);
-}
+});
 
-function deliveryConfirmation (object) {
-  const orderInfo = {
-    event: 'delivered',
-    payload: object,
-  };
-  return orderInfo;
-}
+
+
+
+
+
